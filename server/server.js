2ff3ef4ed0ca9e625194e4fc192bd3d1eabd4634
joostyn <justyn.helgeson@gmail.com>
2022-08-28 17:58:13 -0500

    Inserted server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const path = require('path');
-const {} = require('');
+const PORT = process.env.PORT || 3333;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
